2779963754e62ba53e6b60f58ee35a9588b812b3
Lars Lubkoll <lars.lubkoll@posteo.de>
2016-06-05 00:30:10 +0200

    Fix stl-headers

--- test/sbo/interface.hh
+++ test/sbo/interface.hh
@@ -4,6 +4,17 @@
 #include <cassert>
 #include <memory>
 #include <utility>
+#include <array>
+#include <cassert>
+#include <cstddef>
+#include <type_traits>
+#include <utility>
+
+
+#if defined(_MSC_VER) && _MSC_VER == 1800
+#define noexcept
+#define alignof __alignof
+#endif
 
 
 namespace SBO {
