0b2bd4e6f30aa9cc6d3703722e91b7f44584e1b0
Lars Lubkoll <lars.lubkoll@posteo.de>
2016-06-04 23:10:26 +0200

    Add stl-headers

---
+++ headers/sbo_cow.hpp
@@ -0,0 +1,12 @@
+#include <array>
+#include <atomic>
+#include <cassert>
+#include <cstddef>
+#include <type_traits>
+#include <utility>
+
+
+#if defined(_MSC_VER) && _MSC_VER == 1800
+#define noexcept
+#define alignof __alignof
+#endif
